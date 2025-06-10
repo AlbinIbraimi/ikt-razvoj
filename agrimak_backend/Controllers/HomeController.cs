c020093e11374dbb336fb976d862e8e44f99c464
Albin Ibraimi <albinibraimi@outlook.com>
2025-06-10 14:36:35 +0200

    setup cors policy

---
+++ agrimak_backend/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace agrimak_backend.Controllers
+{
+    [Route("home")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        [Route("test")]
+        public  JsonResult TestEndpoint()
+        {
+            return new JsonResult("test");
+        }
+    }
+}
--- agrimak_backend/Program.cs
+++ agrimak_backend/Program.cs
@@ -1,12 +1,22 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors("AllowAll");
 
 app.UseHttpsRedirection();
 
