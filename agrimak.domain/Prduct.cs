2fa173dfc8d51dae3401759f53a48be9557aac35
Albin Ibraimi <albinibraimi@outlook.com>
2025-06-10 14:45:37 +0200

    add domain class library

---
+++ agrimak.domain/Prduct.cs
@@ -0,0 +1,11 @@
+﻿namespace agrimak_domain
+{
+    public class Prduct
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public byte[] Image { get; set; }
+        public double Price { get; set; }
+        public Category Category { get; set; }
+    }
+}
